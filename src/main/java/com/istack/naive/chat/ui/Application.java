41c34ce6ac37552a59e900b77536e2532e29a6d2
qq_41810415 <qq_41810415@gitcode.net>
2023-07-28 00:04:27 +0800

    author:yangmingjun UI效果修正

--- src/main/java/com/istack/naive/chat/ui/Application.java
+++ src/main/java/com/istack/naive/chat/ui/Application.java
@@ -1,15 +1,26 @@
 package com.istack.naive.chat.ui;
 
-import com.istack.naive.chat.ui.view.Login;
+import com.istack.naive.chat.ui.view.login.ILoginMethod;
+import com.istack.naive.chat.ui.view.login.LoginController;
 import javafx.stage.Stage;
 
+/**
+ * 窗口          Stage
+ * -场景       Scene
+ * -布局     stackPane
+ * -控件   Button
+ */
 public class Application extends javafx.application.Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Login login = new Login();
-        login.show();
+        ILoginMethod login = new LoginController((userId, userPassword) -> {
+            System.out.println("登陆 userId：" + userId + "userPassword：" + userPassword);
+        });
+
+        login.doShow();
     }
+
 
     public static void main(String[] args) {
         launch(args);
