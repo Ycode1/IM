07c894850f26acffc48c8190d207da60a328ee07
qq_41810415 <qq_41810415@gitcode.net>
2023-07-30 22:21:47 +0800

    author:yangmingjun UI功能-修复bug

--- itstack-naive-chat-ui/src/test/java/org/itstack/navice/test/APITest.java
+++ itstack-naive-chat-ui/src/test/java/org/itstack/navice/test/APITest.java
@@ -1,12 +1,10 @@
 package org.itstack.navice.test;
 
-/**
- * 测试
- */
-public class ApiTest {
+public class APITest {
 
     public static void main(String[] args) {
         System.out.println("hi!");
     }
 
 }
+
